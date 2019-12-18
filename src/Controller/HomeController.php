afbb8219e4b08e676937e2c15df1a947d67fe85b
ThaisR <thais.r@live.fr>
2019-12-18 15:37:46 +0100

    plouf

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class HomeController {
+class HomeController extends AbstractController {
 
     /**
      * @Route("/home", name="home")
+     * @param Environment $twig
+     * @return
      */
-    public function index() {
-        $content = "AirBnB Host - Social Network";
+    public function index(Environment $twig) {
+        $content = $this->render('home/index.html.twig');
             return new Response($content);
     }
 }
