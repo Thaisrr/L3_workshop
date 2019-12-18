fa0eea8c64280bf206aa19dcaec987895e9c544c
ThaisR <thais.r@live.fr>
2019-12-18 16:57:38 +0100

    commit de brun

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Entity\User;
-use http\Env\Request;
+use App\Form\ArticleFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends AbstractController
 {
@@ -16,6 +22,36 @@ class ArticleController extends AbstractController
     {
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    /**
+     * @Route("/create-article", name="create-article")
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function createArticle(Request $request) {
+        $article = new Article();
+        $form = $this->createForm(ArticleFormType::class, $article);
+
+        $form->add('ajouter', SubmitType::class, [
+            'label' => 'Ajouter',
+            'attr' => array( 'class' => 'btn btn-outline-info')
+        ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form.getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article');
+        }
+        $form->handleRequest($request);
+
+
+        return $this->render('article/form.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
