20e3a3e0a79ff073bf9b0939914953b7b4b88c6c
MonsieurWai <monsieurwai@gmail.com>
2019-12-20 14:19:24 +0100

    finalisation hp

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,7 +18,42 @@ class HomeController extends AbstractController {
      * @param Environment $twig
      * @return
      */
-    public function index(Environment $twig, Security $security) {
+    public function index(Environment $twig, Security $security, string $param = "") {
+
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+        $events = [];
+        $plans = [];
+        $posts = [];
+
+        foreach($articles as $art)  {
+            switch ($art->getSubject()->getName()) {
+                case 'Evénements' :
+                    array_push($events, $art);
+                    break;
+                case 'Bons Plans' :
+                    array_push($plans, $art);
+                    break;
+                case 'Articles':
+                    array_push($posts, $art);
+            }
+        }
+
+
+        switch ($param) {
+            case "event":
+                $articles = $events;
+                break;
+            case "plan":
+                $articles = $plans;
+                break;
+            case "article":
+                  $articles = $posts;
+                break;
+            default:
+                $articles = $repo->findAll();
+        }  
+
         $repo = $this->getDoctrine()->getRepository(User::class);
         $users = $repo->findAll();
 
@@ -26,7 +61,8 @@ class HomeController extends AbstractController {
         $logements = $repoLog->findAll();
 
         return $this->render('home/index.html.twig', [
-            'users' => $users
+            'users' => $users,
+            'articles' => $articles,
         ]);
     }
 }
